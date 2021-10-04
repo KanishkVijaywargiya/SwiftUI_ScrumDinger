4295a57d7efc30fd788e16596ab9d8812dd5186d
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-10-04 19:15:03 +0530

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by KANISHK VIJAYWARGIYA on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
